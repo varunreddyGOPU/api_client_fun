557bc34cec7b36d0e2c74f73326eef3256fdd6ae
Chris Lamb <lamb.chrisr@gmail.com>
2020-08-04 10:06:52 -0400

    Add a failing test.

--- test/api_client_fun_test.rb
+++ test/api_client_fun_test.rb
@@ -8,4 +8,11 @@ class ApiClientFunTest < Minitest::Test
   def test_it_does_something_useful
     assert false
   end
+
+  # Feel free to change this.
+  def test_returns_profile
+    profile = ApiClientFun.profile_for_name('Philip J Fry')
+    expected_profile = { age: 25, gender: "M", planet: "Earth", species: "Human", status: "Alive" }
+    assert_equal(expected_profile, profile)
+  end
 end
