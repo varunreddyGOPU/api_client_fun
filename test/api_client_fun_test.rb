bd2539d01268ae467923432a21ea77766ca7620d
varunreddyGOPU <96337511+varunreddyGOPU@users.noreply.github.com>
2022-04-08 17:39:58 +0530

    Update api_client_fun_test.rb

--- test/api_client_fun_test.rb
+++ test/api_client_fun_test.rb
@@ -13,6 +13,21 @@ class ApiClientFunTest < Minitest::Test
   def test_returns_profile
     profile = ApiClientFun.profile_for_name('Philip J Fry')
     expected_profile = { age: 25, gender: "M", planet: "Earth", species: "Human", status: "Alive" }
+    
+    profile = ApiClientFun.profile_for_name('Turanga Leela')
+    expected_profile = { age:25,gender:"F",planet:"Earth",species:"Mutant, Human",status:"Alive"}
+    
+    profile = ApiClientFun.profile_for_name('Bender Bending Rodríguez')
+    expected_profile = { age:4,gender:"M",planet:"Earth",species:"Robot",status:"Alive" }
+    
+    profile = ApiClientFun.profile_for_name('Planet Express Ship')
+    expected_profile = { age:"N/A",gender:"N/A",planet:"Earth",species:"Robotic Spacecraft",status:"N/A"}
+    
+    profile = ApiClientFun.profile_for_name('Dr. John A. Zoidberg Ph.D')
+    expected_profile = {age:86,gender:"M",planet:"Decapod 10",species:"Decapodian",status:"Alive"}
+    
+    profile = ApiClientFun.profile_for_name('Zapp Brannigan')
+    expected_profile = { age:28,gender:"M",planet:"Earth",species:"Human",status:"Alive" }
     assert_equal(expected_profile, profile)
   end
 end
